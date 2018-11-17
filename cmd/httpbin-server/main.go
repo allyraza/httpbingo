f35df2d07cd56a62d874977aa8d47af590d28f68
alrazi <saytoally@hotmail.com>
2018-11-17 22:06:35 +0400

    Add a server binary

---
+++ cmd/httpbin-server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/allyraza/httpbingo"
+)
+
+func main() {
+	config := &httpbin.Config{}
+
+	flag.StringVar(&config.Filepath, "config", "config.json", "Config file for httpbin-server.")
+	flag.Parse()
+
+	config.ParseFile()
+
+	log.Printf("Starting server on %v\n", config.Addr)
+	h := httpbin.New(config)
+
+	server := http.Server{
+		Addr:    config.Addr,
+		Handler: h,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+
+	log.Printf("Shutdown signal received, quiting...")
+	server.Shutdown(context.Background())
+}
